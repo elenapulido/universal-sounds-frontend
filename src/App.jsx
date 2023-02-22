4d4149f612143cec9186471f8dda20c308c91fd9
AleMCuitino <ale.morales11@gmail.com>
2023-02-22 12:45:15 +0100

    Adedd react router, tailwind

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Router from "./routes/Routes";
+
+function App() {
+  return (
+  <>
+    <Router/>
+  </> 
+  )
+}
+
+export default App;
---
+++ src/views/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+
+export default function Home() {
+
+    return (
+        <React.Fragment>
+            <h1>Hola Mundo</h1>
+        </React.Fragment>
+    );
+}
