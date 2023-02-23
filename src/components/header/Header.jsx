2589e6601653a4ae81ceec38086b01bb718772d2
AleMCuitino <ale.morales11@gmail.com>
2023-02-23 14:40:38 +0100

    Fixed header and searchbar

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,26 +1,16 @@
-import React from 'react'
-import logo from '../../assets/logo_records-01.png'
-import icono from'../../assets/bolsa.png'
+import React from 'react';
+import logo from '../../assets/logo_records-01.png';
+import icono from'../../assets/bolsa.png';
+import SearchBar from '../search-bar/SearchBar';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
     <header className="bg-white-500 text-white">
       <div className="container mx-auto flex items-center justify-between py-4">
-        <img src={logo}  className="w-29 h-24" alt="Descripción de la imagen"/ >
-        {/* <form action="#" method="GET">
-          <div className="flex items-center border rounded-md border-white bg-white">
-            <input type="text" name="search" placeholder="Buscar..." className="py-2 px-4 w-full rounded-md" />
-            <button type="submit" className="bg-gray-500 text-white py-2 px-4 rounded-md">Buscar</button>
-          </div>
-         </form> */}
-          
-        <Link to="/cart"> <img src={icono} className="w-10 h-13" alt="Descripción de la imagen" /></Link>   
-      </div>
-      <div class="border border-solid w-64 h-0 my-4"></div>
- 
+        <img src={logo}  className="w-29 h-24" alt="Descripción de la imagen"/>
         <SearchBar />
-        <img src={icono} className="w-19 h-14" alt="Descripción de la imagen"/>
+        <Link to="/cart"><img src={icono} className="w-10 h-13" alt="Descripción de la imagen" /></Link>   
       </div>
     </header>
   )
--- src/views/Home.jsx
+++ src/views/Home.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '../components/header/Header';
-import Navbar from '../components/header/Header';
 
 
 
@@ -9,7 +8,6 @@ export default function Home() {
     return (
         <div>
             <Header />
-            <Navbar />
         </div>
     );
 }
